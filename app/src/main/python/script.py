8ace8c204296e27283857cd5d2bae4e30e0362bd
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2024-09-17 14:10:14 +0700

    Fix file format

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -18,7 +18,7 @@ def download_post_from_link(url):
                 file.write(get_content.content)
     else:
         random_hash = str(secrets.token_hex(nbytes=16))
-        get_content = requests.get(download_url[0])
+        get_content = requests.get(download_url[1])
         mime = get_content.headers.get("Content-Type")
         ext = mime.split("/")[0]
         with open(f"/sdcard/TikMedia/{random_hash}.{ext}", "wb") as file:
