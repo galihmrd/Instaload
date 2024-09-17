379261eb41d7a9b1a94f422e6e17ad0328ab9fd7
GalihMrd <69835093+galihmrd@users.noreply.github.com>
2024-09-17 14:15:46 +0700

    Fix py script

--- app/src/main/python/script.py
+++ app/src/main/python/script.py
@@ -13,13 +13,13 @@ def download_post_from_link(url):
             random_hash = str(secrets.token_hex(nbytes=16))
             get_content = requests.get(dl_url)
             mime = get_content.headers.get("Content-Type")
-            ext = mime.split("/")[0]
+            ext = mime.split("/")[1]
             with open(f"/sdcard/TikMedia/{random_hash}.{ext}", "wb") as file:
                 file.write(get_content.content)
     else:
         random_hash = str(secrets.token_hex(nbytes=16))
-        get_content = requests.get(download_url[1])
+        get_content = requests.get(download_url[0])
         mime = get_content.headers.get("Content-Type")
-        ext = mime.split("/")[0]
+        ext = mime.split("/")[1]
         with open(f"/sdcard/TikMedia/{random_hash}.{ext}", "wb") as file:
             file.write(get_content.content)
